589cda315c3e3236f0e52bb0a0ef0eafa4c03799
taguaоу <taguaje386@gmail.com>
2019-05-20 00:38:08 +0300

    timeline how

--- wedding/settings.py
+++ wedding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%b&if!7jyja*c-jg#*d_*=gmyy-k)_l36mu_64*ql7+_z$9uce'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.12']
 
 
 # Application definition
