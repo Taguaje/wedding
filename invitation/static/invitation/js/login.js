995f824f6875910fa6bca959b0eead344806e885
taguaоу <taguaje386@gmail.com>
2019-06-04 01:10:28 +0300

    timeline how

--- invitation/static/invitation/js/login.js
+++ invitation/static/invitation/js/login.js
@@ -1,6 +1,10 @@
 function updateLogin() {
     var nameInput = document.getElementById('name');
     var secondNameInput = document.getElementById('second_name');
-    document.getElementById('login_id').value = (nameInput.value + secondNameInput.value).replace(/\s/g, '');
-    document.getElementById('id_password').value = secondNameInput.value.replace(/\s+/g, '');
+    document.getElementById('login_id').value = (capitalizeFirstLetter(nameInput.value) + capitalizeFirstLetter(secondNameInput.value)).replace(/\s/g, '');
+    document.getElementById('id_password').value = capitalizeFirstLetter(secondNameInput.value.replace(/\s+/g, ''));
+}
+
+function capitalizeFirstLetter(s) {
+    return s[0].toUpperCase() + s.slice(1);
 }
